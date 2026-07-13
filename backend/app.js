029bf74d210d47678e19a3adc062e41419f22867
Ifandi <mohifan61@gmail.com>
2026-07-13 14:11:30 +0700

    Initial project setup with Svelte and Express

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+// koneksi database
+require("./config/database");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        status: "success",
+        message: "Backend Perpustakaan berjalan!"
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server berjalan di http://localhost:${PORT}`);
+});
